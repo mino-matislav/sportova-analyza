11ce42932dd1cc5b5a50f8d2efae89bd140d56b2
mino-matislav <mino.matislav@gmail.com>
2026-06-28 16:28:43 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'sportova-analyza-v2';
-const urlsToCache = ['/','/index.html','https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700&display=swap'];
+const CACHE_NAME = 'sportova-analyza-v3';
+const urlsToCache = ['/','/index.html'];
 self.addEventListener('install', e => { e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(urlsToCache))); });
 self.addEventListener('fetch', e => { e.respondWith(caches.match(e.request).then(r => r || fetch(e.request))); });
